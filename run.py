7e03f755e821c0305fecc536cdca344eb153e6a2
alessiamarcolini <98marcolini@gmail.com>
2020-04-15 16:58:51 +0200

    Fix environment file

--- run.py
+++ run.py
@@ -1,13 +1,15 @@
-print("partito")
 import yaml
 
 from data_aug.dataset_wrapper import DataSetWrapper
 from simclr import SimCLR
 
-print("ciao dal main")
-config = yaml.load(open("config.yaml", "r"), Loader=yaml.FullLoader)
-print("ok yaml")
-dataset = DataSetWrapper(config["batch_size"], **config["dataset"])
-print("ok dataset")
-simclr = SimCLR(dataset, config)
-simclr.train()
+
+def main():
+    config = yaml.load(open("config.yaml", "r"), Loader=yaml.FullLoader)
+    dataset = DataSetWrapper(config["batch_size"], **config["dataset"])
+    simclr = SimCLR(dataset, config)
+    simclr.train()
+
+
+if __name__ == "__main__":
+    main()
